72043230b710424a6e1cf02c5ca3c51dd87b17a9
Lu-jiajun <Lu_jiajun@outlook.com>
2024-01-30 01:11:39 +0800

    2024-1-30

---
+++ api-passenage/src/main/java/fun/lujiajun/apipassenger/ApiPassengerApplication.java
@@ -0,0 +1,12 @@
+package fun.lujiajun.apipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPassengerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
+
+}
